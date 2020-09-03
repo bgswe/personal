c3ea9814ce0ded53fdd4f732e8f9676577ace42d
Bryce Gallion <bg@Bryces-MacBook-Pro.local>
2020-09-02 20:44:46 -0700

    Remove CRA defaults, unecessary files.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import teal from '@material-ui/core/colors/teal';
+import grey from '@material-ui/core/colors/grey';
+
+import Navbar from './components/Navbar'
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: teal[900],
+        },
+        secondary: {
+            main: grey[50],
+        },
+    },
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <Navbar />
+        </ThemeProvider>
+    );
 }
 
 export default App;
